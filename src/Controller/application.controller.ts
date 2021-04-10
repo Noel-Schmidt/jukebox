6f6e37ffd6737b60d0d2a2d255ee5892afc3263f
noel-schmidt <noel_schmidt@outlook.de>
2021-04-10 20:35:05 +0200

    Added Message Event

--- src/Controller/application.controller.ts
+++ src/Controller/application.controller.ts
@@ -1,6 +1,7 @@
-import { Client } from "discord.js"
+import {Client, Message, MessageEmbed} from "discord.js"
 import {CommandHandler} from "../Command/Handle/comand.handler";
 import {HelpCommand} from "../Command/help.command";
+import {ENVIRONMENT} from "../main";
 
 export class ApplicationController {
     instance: Client;
@@ -25,6 +26,30 @@ export class ApplicationController {
     setup() {
         this.commandHandler = new CommandHandler();
         this.commandHandler.registerCommand(new HelpCommand(this.commandHandler));
+
+        this.instance.on("message", (event: Message) => {
+            const author = event.author
+            const message = event.content
+            const guild = event.guild
+            const channel = event.channel
+
+            if(message.includes(ENVIRONMENT.get('BOT_COMMAND_PREFIX'))) {
+                var args: Array<string> = message.split(" ")
+                if(args[0].startsWith(ENVIRONMENT.get('BOT_COMMAND_PREFIX'))) {
+                    const cmd: string = args[0].replace(ENVIRONMENT.get('BOT_COMMAND_PREFIX'),"")
+                    args = args.filter(str => str != ENVIRONMENT.get('BOT_COMMAND_PREFIX')+cmd)
+                    if(!this.commandHandler.handle(cmd, author, event, channel, args, guild)) {
+                        event.delete()
+                        const messageEmbed = new MessageEmbed({
+                            title: "Fehler",
+                            description: "Dieser Command existiert nicht",
+                            color: "RED"
+                        })
+                        channel.send(messageEmbed)
+                    }
+                }
+            }
+        });
     }
 
 }
